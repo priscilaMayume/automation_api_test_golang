727b4ec8757bac52f6e3de4a804350fa596e00c5
priscilaMayume <prisccila@gmail.com>
2024-05-24 09:25:20 -0300

    main.go improvement

--- main.go
+++ main.go
@@ -1,34 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 7
+	// print welcome message
+	intro()
 
-	// Chama a função isPrime e imprime a mensagem retornada
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	// create a channel to indicate when the user wants to quit
+	doneChan := make(chan bool)
+
+	// start a goroutine to read input and run program
+	go readUserInput(doneChan)
+
+	// wait for the signal to quit
+	<-doneChan
+
+	// say goodbye
+	fmt.Println("Goodbye.")
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+		if done {
+			doneChan <- true
+			return
+		}
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// read user input
+	scanner.Scan()
+
+	// check to see if the user wants to quit
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// try to convert what the user typed into an int
+	numToCheck, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return "Please enter a whole number!", false
+	}
+
+	_, msg := isPrime(numToCheck)
+
+	return msg, false
+}
+
+func intro() {
+	fmt.Println("Is it Prime?")
+	fmt.Println("---------")
+	fmt.Println("Enter a whole number, and we'll tell you if it's a prime number or not. Enter q to quit.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("-> ")
 }
 
 func isPrime(n int) (bool, string) {
-	// 0 e 1 não são números primos por definição
+	// 0 and 1 are not prime numbers by definition
 	if n == 0 || n == 1 {
 		return false, fmt.Sprintf("%d is not prime, by definition!", n)
 	}
 
-	// Números negativos não são primos
+	// Negative numbers are not prime
 	if n < 0 {
 		return false, "Negative numbers are not prime, by definition!"
 	}
 
-	// Usa o operador de módulo repetidamente para verificar se é um número primo
+	// Use the modulus operator repeatedly to check for prime
 	for i := 2; i <= n/2; i++ {
 		if n%i == 0 {
-			// Não é um número primo
+			// Not a prime number
 			return false, fmt.Sprintf("%d is not a prime number because it is divisible by %d", n, i)
 		}
 	}
 
-	// É um número primo
+	// Is a prime number
 	return true, fmt.Sprintf("%d is a prime number!", n)
 }
